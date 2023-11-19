61f1cf4feb23e6441e1ccf1d0eae873d4802003c
Prathamesh Sonawane <85408924+Prathya3637@users.noreply.github.com>
2023-11-19 16:13:06 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,5 @@
+let header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+var items = document.getElementsByClassName('title');
+items[0].style.fontWeight = 'bold';
+items[0].style.color = 'green';
