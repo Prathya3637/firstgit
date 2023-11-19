93def19e29bc179490736e2406346a505b2b6757
Prathamesh Sonawane <85408924+Prathya3637@users.noreply.github.com>
2023-11-19 16:27:42 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -3,3 +3,8 @@ header.style.borderBottom = 'solid 3px #000';
 var items = document.getElementsByClassName('title');
 items[0].style.fontWeight = 'bold';
 items[0].style.color = 'green';
+var lists = document.getElementsByClassName('list-group-item');
+lists[2].style.backgroundColor = 'green';
+for(let i =0; i<lists.length;i++){
+    lists[i].style.fontWeight = 'bold';
+}
